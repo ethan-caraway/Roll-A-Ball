a73c5541dbbef37b238194c38597761156fe8688
ethan <ethan@flightpaperstudio.com>
2026-04-10 11:31:39 -0500

    Added player movement

---
+++ Roll A Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+	// The Rigidbody component attached to the player
+	Rigidbody rb;
+
+	// The movement input values along the X and Y axes
+	float inputX;
+	float inputY;
+
+	// The speed at which the player will move
+	float speed = 10;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+		// Get and store the Rigidbody component attached to the player
+		rb = GetComponent<Rigidbody> ( );
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		// Create a 3D vector using the X and Y input values
+		Vector3 movement = new Vector3 ( inputX, 0f, inputY );
+
+		// Apply a physics force to the Rigidbody to move the player
+		rb.AddForce ( movement * speed );
+    }
+
+	void OnMove ( InputValue moveValue )
+	{
+		// Convert the movement input value into a 2D vector
+		Vector2 moveVector = moveValue.Get<Vector2> ( );
+
+		// Store the X and Y input values for the movement
+		inputX = moveVector.x;
+		inputY = moveVector.y;
+	}
+}
